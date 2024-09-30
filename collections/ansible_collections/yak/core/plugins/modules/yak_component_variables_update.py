9243ca4652d81783f17300e1d1ce4f8ea3e69af6
Ryan BADAÏ <ryan.badai@dbi-services.com>
2024-09-30 16:34:06 +0200

    feat: add module to update component variables
    Issue: yak_runner#64

---
+++ collections/ansible_collections/yak/core/plugins/modules/yak_component_variables_update.py
@@ -0,0 +1,188 @@
+#!/usr/bin/python
+
+from __future__ import absolute_import, division, print_function
+from ansible.errors import AnsibleError
+import requests
+import os
+
+__metaclass__ = type
+
+DOCUMENTATION = r'''
+---
+module: yak_component_variables_update
+short_description: Update YAK component variables
+description:
+    - This module updates component variables in the YAK system using a GraphQL query.
+options:
+    component_name:
+        description: The name of the component to update
+        required: true
+        type: str
+    component_variables:
+        description: List of variables to update
+        required: true
+        type: list
+        elements: dict
+        options:
+            variable:
+                description: The name of the variable to update
+                required: true
+                type: str
+            value:
+                description: The new value for the variable
+                required: true
+                type: str
+'''
+
+EXAMPLES = r'''
+- name: Update component variables
+  yak.core.yak_component_variables_update:
+    component_name: "postgres1"
+    component_variables:
+      - variable: "version"
+        value: "pv_postgres_target_version"
+'''
+
+RETURN = r'''
+message:
+    description: The output message that the module generates
+    type: str
+    returned: always
+'''
+
+from ansible.module_utils.basic import AnsibleModule
+
+# Define the available arguments/parameters that a user can pass to the module
+module_args = dict(
+component_name=dict(type='str', required=True),
+variable_name=dict(type='str', required=True),
+value=dict(type='str', required=True)
+)
+
+# Seed the result dict in the object
+result = dict(
+    changed=False,
+    message=''
+)
+
+# Create the module object
+module = AnsibleModule(
+    argument_spec=module_args,
+    supports_check_mode=True
+)
+
+def graphQLRequest(graphql_request, graphql_request_variables):
+
+    response = requests.post(
+        url=os.environ.get('YAK_ANSIBLE_TRANSPORT_URL'),
+        headers={
+            "Authorization": "Bearer {}".format(os.environ.get('YAK_ANSIBLE_HTTP_TOKEN')),
+            "Content-Type": "application/json",
+        },
+        json={
+            "query": graphql_request,
+            "variables": graphql_request_variables,
+        },
+    )
+    if response.status_code != 200:
+        raise AnsibleError(f"API error: {response}\n{response.text}")
+    if "errors" in response.json():
+        raise AnsibleError("GraphQL error: {}\n".format(response.json()["errors"][0]["message"]))
+    if graphql_request.lstrip().startswith('query'):
+        return response.json()["data"]
+
+def get_variables_query(component_name):
+    graphql_request = """
+    query GetComponentVariables($name: String!) {
+    vComponents(condition: {name: $name}) {
+        edges {
+        node {
+            basicVariables
+            advancedVariables
+            id
+        }
+        }
+    }
+    }
+    """
+    
+    graphql_request_variables = {"name": component_name} 
+    query_result = graphQLRequest(graphql_request, graphql_request_variables)["vComponents"]["edges"][0]["node"]
+    basic_variables = query_result.get("basicVariables")
+    advanced_variables = query_result.get("advancedVariables")
+    return basic_variables, advanced_variables
+
+def update_variables_query(component_name, basic_variables, advanced_variables):
+    graphql_request = """
+    mutation UpdateComponentVariables($pAdvancedVariables: JSON = "", $pBasicVariables: JSON = "", $pComponentName: String = "") {
+        componentUpdate(
+            input: {pAdvancedVariables: $pAdvancedVariables, pBasicVariables: $pBasicVariables, pComponentName: $pComponentName}
+        ) {
+            clientMutationId
+            integer
+        }
+    }
+    """
+    
+    graphql_request_variables = {
+        "pComponentName": component_name,
+        "pBasicVariables": basic_variables,
+        "pAdvancedVariables": advanced_variables
+    } 
+    query_result = graphQLRequest(graphql_request, graphql_request_variables)
+    return query_result
+
+def run_module():
+    # If the user is working with this module in only check mode we do not
+    # want to make any changes to the environment, just return the current
+    # state with no modifications
+    if module.check_mode:
+        module.exit_json(**result)
+
+    # Implement the logic to update the component variables using GraphQL
+    try:
+        component_name = module.params['component_name']
+        variable_name = module.params['variable_name']
+        variable_value= module.params['value']
+
+        # Here you would implement the function to update the database using GraphQL
+        # For now, we'll just leave it as a placeholder
+        def update_component_variable(component_name, variable_name, variable_value):
+            # Implement your GraphQL query and database update logic here
+            # This is where you'd make the actual changes to the database
+            basic_variables, advanced_variables = get_variables_query(component_name)
+            
+            if variable_name in advanced_variables:
+                if advanced_variables.get(variable_name) != variable_value:
+                    advanced_variables[variable_name] = variable_value
+                    update_variables_query(component_name, basic_variables, advanced_variables)
+                    result['changed'] = True
+                    result['message'] = f"Variable named {variable_name} properly modified to {variable_value}"
+                else:
+                    result['message'] = f"Variable named {variable_name} already has the proper value, no changes done."
+            elif variable_name in basic_variables:
+                if basic_variables.get(variable_name) != variable_value:
+                    basic_variables[variable_name] = variable_value
+                    update_variables_query(component_name, basic_variables, advanced_variables)
+                    result['changed'] = True
+                    result['message'] = f"Variable named {variable_name} properly modified to {variable_value}"
+                else:
+                    result['message'] = f"Variable named {variable_name} already has the proper value, no changes done."
+            else:
+                msg = f"Variable named {variable_name} doesn't exists !"
+                module.fail_json(msg=msg)
+
+        update_component_variable(component_name, variable_name, variable_value)
+
+    except Exception as e:
+        module.fail_json(msg=f"Error updating component variables: {str(e)}", **result)
+
+    # In the event of a successful module execution, you will want to
+    # simple AnsibleModule.exit_json(), passing the key/value results
+    module.exit_json(**result)
+
+def main():
+    run_module()
+
+if __name__ == '__main__':
+    main()
